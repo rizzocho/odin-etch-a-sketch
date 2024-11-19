3bc5961c949cb87932a6cb646929de1bc7a656f6
rizzocho <98249535+rizzocho@users.noreply.github.com>
2024-11-18 19:32:41 -0800

    create grid of flex divs

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 const parent = document.querySelector("#container");
-const child = document.createElement("div");
+const parentWidth = 500;
+parent.style.width = `${parentWidth}px`;
+const square = 3;
+const squareWidth = (parentWidth - (square * 3)) / square;
 
+
+for (let i = 0; i < square; i++) {
+    for (let j = 0; j < square; j++) {
+        newSquare();
+    }
+
+}
+
+function newSquare() {
+    const child = document.createElement("div");
+    child.className = "square";
+    child.style.height = `${squareWidth}px`;
+    child.style.width = `${squareWidth}px`;
+    child.style.border = '1px solid black';
+    parent.appendChild(child);   
+}
